a02ccf39036511041b8ec06979a2b19bdf6ef0c9
emil <emil@emil-MacBookPro>
2016-11-22 12:54:59 +0100

    GUI changes

--- src/concordia/GUIController.java
+++ src/concordia/GUIController.java
@@ -10,15 +10,35 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Side;
 import javafx.scene.control.Label;
+import javafx.scene.control.TabPane;
 
 /**
  *
  * @author emilvanamerongen
  */
 public class GUIController implements Initializable {
+   
+    @FXML
+    TabPane datasetstabpane;
     
-    
+    @FXML
+    private void datasetswitchtab(ActionEvent event){
+        String source = event.getSource().toString();
+        if (source.contains("new")){
+            datasetstabpane.getSelectionModel().select(1);
+        }
+        else if (source.contains("add")){
+            datasetstabpane.getSelectionModel().select(2);
+        }
+        else if (source.contains("")){
+        
+        }
+        else if (source.contains("")){
+        
+        }
+    }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
