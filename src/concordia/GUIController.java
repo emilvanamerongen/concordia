1acf2d2e7c1204dcde1a7ea3f9cd16c2e9f2860e
emil <emil@emil-MacBookPro>
2017-01-17 13:09:49 +0100

    Clean slate

--- src/concordia/GUIController.java
+++ src/concordia/GUIController.java
@@ -5,8 +5,6 @@
  */
 package concordia;
 
-import Objects.dataset;
-import Objects.loadbar;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
@@ -44,7 +42,7 @@ import javafx.util.Duration;
 public class GUIController implements Initializable {
    //// datasets page
     @FXML
-    TabPane datasetstabpane;
+    TabPane filespane;
     @FXML
     ListView datasetlist;
     @FXML
@@ -95,33 +93,17 @@ public class GUIController implements Initializable {
     
 
             
-    //parser variables
-    Timeline timeline = new Timeline(new KeyFrame(Duration.millis(1000), ae -> parserloop()));
-    public static Boolean timelineactive = true;
-    public static String parserloadlabel;
-    public static loadbar parserloadbar = new loadbar();
-    public static String parserexample1;
-    public static String parserexample2;
-    public File importfiletemp; 
     
-    
-    
-    HashMap<String, dataset> datasets = new HashMap<>();
-    CreateDatabase newdatabase = new CreateDatabase();
+
     
     @FXML
-    private void databaseButton(ActionEvent event) throws SQLException{
-        newdatabase.createDataTables();
-    }
-    
-    @FXML
-    private void datasetswitchtab(ActionEvent event){
+    private void switchtab(ActionEvent event){
         String source = event.getSource().toString();
         if (source.contains("new")){
-            datasetstabpane.getSelectionModel().select(1);
+            filespane.getSelectionModel().select(1);
         }
         else  if (source.contains("add")){
-            datasetstabpane.getSelectionModel().select(2);
+            filespane.getSelectionModel().select(2);
         }
         else if (source.contains("")){
             
@@ -131,131 +113,10 @@ public class GUIController implements Initializable {
         }
     }
     
-    @FXML
-    private void adddataset(ActionEvent event){
-        String newsettitle = datasettitle.getText();
-        if (!"".equals(newsettitle)){
-            datasets.put(newsettitle, new dataset());
-            datasettitle.setText("");
-            updateguilistviews();
-            updateguibuttons();
-            datasetstabpane.getSelectionModel().select(0);
-        }
-    }   
-    @FXML
-    private void deletedataset(ActionEvent event){
-        String removetitle = (String) datasetlist.getSelectionModel().getSelectedItem();
-        datasets.remove(removetitle);
-        updateguilistviews();
-        updateguibuttons();
-        datasetstabpane.getSelectionModel().select(0);
-    } 
-    public void updateguilistviews(){
-        ObservableList<String> datasetlistitems =FXCollections.observableArrayList(datasets.keySet());
-        datasetlist.setItems(datasetlistitems);
-    }
-    @FXML
-    public void updateguibuttons(){
-        if (datasetlist.getItems().size() == 0){
-            datasetdeletebutton.setDisable(true);
-            adddatabutton.setDisable(true);
-            blastbutton.setDisable(true);
-            annotationaddbutton.setDisable(true);
-            annotationremovebutton.setDisable(true);
-        }   else {
-            datasetdeletebutton.setDisable(false);
-        }
-        if (datasetlist.getSelectionModel().getSelectedItem() != null){
-            adddatabutton.setDisable(false);
-        } else {
-            adddatabutton.setDisable(true);
-        }
-        updateguilabels();
-    }
-    @FXML
-    public void updateguilabels(){
-        ngsdatalabel.setText((String) datasetlist.getSelectionModel().getSelectedItem());
-    }
-    
-    // add data functions:
-    @FXML
-    public void openNGSfile(ActionEvent event) throws IOException{
-        Stage stage = new Stage();
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Open NGS data File");
-                    fileChooser.getExtensionFilters().addAll(
-                new FileChooser.ExtensionFilter("FASTQ", "*.fastq"),
-                new FileChooser.ExtensionFilter("FASTA", "*.fasta")
-            );
-        importfiletemp = fileChooser.showOpenDialog(stage);
-        if (importfiletemp != null){
-        ngsfilelabel.setText(importfiletemp.getName());
-        }
-    }
-    @FXML
-    public void adddata(ActionEvent event) throws IOException{
-        String[] ngstext = addngstext.getText().split("\\r?\\n");
-        String selecteddataset = (String) datasetlist.getSelectionModel().getSelectedItem();   
-        System.out.println(adddatachoicebox.getValue());
-        if (! ngstext[0].isEmpty()){
-            System.out.println("PARSER MODULE: GO text");
-            dataparser newparser = new dataparser(ngstext, "");
-            newparser.process(headidentifierfield.getText(),forwardfield.getText(),reversefield.getText(),selecteddataset,!directioncheckbox.isSelected(), "MANUAL INPUT");
-            
-        }
-        else if (importfiletemp != null){
-            System.out.println("PARSER MODULE: GO file");
-            dataparser newparser = new dataparser(importfiletemp, "");
-            newparser.process(headidentifierfield.getText(),forwardfield.getText(),reversefield.getText(),selecteddataset,!directioncheckbox.isSelected(),importfiletemp.getName());
-        } else {
-            return;
-        }
-        adddataprogresslabel.setText("importing");
-        parserloadbar.doneproperty().addListener(new ChangeListener(){
-        @Override public void changed(ObservableValue o,Object oldVal, Object newVal){
-            double progress = parserloadbar.getdone();
-            adddataprogressbar.setProgress(parserloadbar.getdone());
-        }
-        });
-        timeline.setCycleCount(Animation.INDEFINITE);
-        timeline.play();
-        datasetstabpane.getSelectionModel().select(3);
-    }      
-    
-    @FXML 
-    public void readdirectiondisable(ActionEvent event){
-        if (directioncheckbox.isSelected()){
-            forwardfield.setDisable(false);
-            reversefield.setDisable(false);
-            forwardtext.setDisable(false);
-            reversetext.setDisable(false);
-        } else{
-            forwardfield.setDisable(true);
-            reversefield.setDisable(true);
-            forwardtext.setDisable(true);
-            reversetext.setDisable(true);
-        }
-    }
-    
-    public void parserloop(){
-        adddataprogresslabel.setText(parserloadlabel);
-        if (timelineactive == false){
-            adddataprogresslabel.setText("import complete");
-            datasetstabpane.getSelectionModel().select(0);
-            System.out.println("parse complete");
-            parserloadlabel = "";
-            adddataprogresslabel.setText(parserloadlabel);
-            timeline.stop();
-            ngsfilelabel.setText("");
-            importfiletemp = null;
-        }
-    }
     //init
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         //TODO - get data from database and load into datasetlist
-        adddatachoicebox.setItems(FXCollections.observableArrayList("FASTq", "FASTA"));
-        adddatachoicebox.getSelectionModel().selectFirst();
         
     }    
     
