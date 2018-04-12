37ad197c69fac00061d8e15b90d53ae182321500
emil <emil@emil-MacBookPro>
2018-04-12 16:16:12 +0200

    changes

--- src/concordia/Concordia.java
+++ src/concordia/Concordia.java
@@ -12,6 +12,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import server.ConcordiaServer;
 
 /**
  *
@@ -20,13 +21,16 @@ import javafx.stage.Stage;
 
 public class Concordia extends Application {
     
-    static Boolean nogui = false;
+    static Boolean server = false;
     static Boolean remote = false;
     public static Boolean serveractive = false;
     
+            
     @Override
     public void start(Stage stage) throws Exception {
-        if (!nogui && !remote){
+        Thread.currentThread().setName("CONCORDIA");
+        System.out.println("-------------------------");
+        if (!server && !remote){
             System.out.println("STARTING CONCORDIA IN LOCAL MODE");
             Parent root = FXMLLoader.load(Concordia.class.getResource("localGUI.fxml"));   
             Scene scene = new Scene(root);
@@ -38,26 +42,30 @@ public class Concordia extends Application {
             Scene scene = new Scene(root);
             stage.setScene(scene);
             stage.show();
-        } else {
-            System.out.println("STARTING CONCORDIA IN SERVER MODE");
-            serveractive = true;
-            ConcordiaServer concordiaserverthread = new ConcordiaServer();
-            concordiaserverthread.start();
-        }
+        } 
     }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // check command line arguments and set server/remote booleans accordingly
         try{
             if (args[0].contains("SERVER")||args[0].contains("server")){
-                nogui = true;
+                server = true;
             } else if (args[0].contains("REMOTE")||args[0].contains("remote")){
                 remote = true;
             }
         } catch (Exception ex){}
-        launch(args);
+        if (server){
+            System.out.println("STARTING CONCORDIA IN SERVER MODE");
+            serveractive = true;
+            ConcordiaServer concordiaserverthread = new ConcordiaServer();
+            concordiaserverthread.start();
+        } else{
+            launch(args);
+        }
+        
     }
     
 }
